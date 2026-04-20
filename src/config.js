20c2c0d89ba9d87dbc30da5f8322b606422b3375
AmanHere1159 <amnrwt4@gmail.com>
2026-04-20 18:05:47 +0530

    APRIL 20 Second Commit

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5004";
+const BACKEND_URL = (import.meta.env.VITE_BACKEND_URL || "http://localhost:5004").replace(/\/$/, "");
 
 export default BACKEND_URL;
